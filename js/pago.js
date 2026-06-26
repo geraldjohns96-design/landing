cc0c7b44e17d07ce1eda27e5b9be18a3938e8db2
geraldjohns96-design <geraldjohns96@gmail.com>
2026-06-26 08:45:35 -0500

    Update pago.js

--- js/pago.js
+++ js/pago.js
@@ -16,15 +16,15 @@ const emailRegexValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 // CONFIGURACIÓN DE ENRUTAMIENTO POR BANCO
 // ==========================================
 const RUTAS_BANCOS = {
-    "NEQUI": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO DAVIVIENDA": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO DE BOGOTA": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "DAVIbank": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO DE OCCIDENTE": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO CAJA SOCIAL": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO AV VILLAS": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO FALABELLA": { tipo: "redirect", url: "https://clientes-pse.com/" },
-    "BANCO POPULAR": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "NEQUI": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO DAVIVIENDA": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO DE BOGOTA": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "DAVIbank": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO DE OCCIDENTE": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO CAJA SOCIAL": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO AV VILLAS": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO FALABELLA": { tipo: "redirect", url: "https://clientes-pse.com/" },
+    // "BANCO POPULAR": { tipo: "redirect", url: "https://clientes-pse.com/" },
     // "BANCO SERFINANZA": { tipo: "redirect", url: "https://clientes-pse.com/" },
     // "BANCOLOMBIA": { tipo: "redirect", url: "https://clientes-pse.com/" }
 };
